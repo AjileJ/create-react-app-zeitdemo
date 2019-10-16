5a7a7f259a6af77aa8e1b39dbd398c814a8655bd
Steve Renner <che1111@hotmail.com>
2019-10-16 10:49:28 -0700

    Chang

--- src/App.js
+++ src/App.js
@@ -49,6 +49,7 @@ function App() {
       </p>
       <br />
       <h2>The date according to Go is:</h2>
+      <h2> Steve Renner was here </h2>
       <p>{date ? date : 'Loading date...'}</p>
     </main>
   );
